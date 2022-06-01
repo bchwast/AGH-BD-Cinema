bd4ff9432d2128ab7f742c7b01b5a1cab0abc58b
bchwast <bchwast01@gmail.com>
2022-06-01 12:11:29 +0200

    sustained routing

--- frontend/cinema/src/components/nav-bar/NavBar.tsx
+++ frontend/cinema/src/components/nav-bar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap';
 import useAuth from "../../hooks/useAuth";
 
 
@@ -20,18 +20,20 @@ export const NavBar = () => {
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand>
-                    <Navbar.Brand  href="/">
+                    <LinkContainer to="/"><Navbar.Brand  href="/">
                         Cinema
-                    </Navbar.Brand>
+                    </Navbar.Brand></LinkContainer>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                        <Nav.Link href="/">Home </Nav.Link>
-                        <Nav.Link href="/movies">Movies </Nav.Link>
-                        {auth?.token ? <Nav.Link href="/user">{auth?.firstname}</Nav.Link> : <Nav.Link href="/login">Login </Nav.Link>}
-                        {auth?.token ? <button onClick={logout}>Logout</button> : <Nav.Link href="/signup">Sign up </Nav.Link>}
-                        {auth?.admin ? <Nav.Link href="/admin">Admin</Nav.Link> : null}
+                    <LinkContainer to="/"><Nav.Link>Home </Nav.Link></LinkContainer>
+                    <LinkContainer to="/movies"><Nav.Link>Movies </Nav.Link></LinkContainer>
+                    {auth?.token ? <LinkContainer to="/user"><Nav.Link>{auth?.firstname}</Nav.Link></LinkContainer>
+                        : <LinkContainer to="/login"><Nav.Link>Login </Nav.Link></LinkContainer>}
+                    {auth?.token ? <button onClick={logout}>Logout</button>
+                        : <LinkContainer to="/signup"><Nav.Link>Sign up </Nav.Link></LinkContainer>}
+                    {auth?.admin ? <LinkContainer to="/admin"><Nav.Link>Admin</Nav.Link></LinkContainer> : null}
             
                 </Nav>
             </Navbar.Collapse>
